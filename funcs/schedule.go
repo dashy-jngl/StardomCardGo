3d4767f4e9bc44c035928f9b185f65f1b9080338
dash <hash.chisako@gmail.com>
2026-03-23 11:15:00 +1030

    parse date

--- funcs/schedule.go
+++ funcs/schedule.go
@@ -18,20 +18,28 @@ type MatchCard struct {
 }
 
 func ParseCard(url string) (MatchCard, error) {
+    // Make an HTTP GET request to the URL
     resp, err := http.Get(url)
     if err != nil {
         return MatchCard{}, err
     }
     defer resp.Body.Close()
 
+    // Check if the request was successful
     doc, err := goquery.NewDocumentFromReader(resp.Body)
     if err != nil {
         return MatchCard{}, err
     }
 
+    // Extract the title of the match card
     title := doc.Find("h1.match_head_title").First().Text()
     title = strings.TrimSpace(title)
 
+    //
+    date := doc.Find("p.date").First().Text()
+    date = strings.TrimSpace(date)
+
     fmt.Println("Title:", title)
-    return MatchCard{Title: title}, nil
+    fmt.Println("Date:", date)
+    return MatchCard{Title: title, Date: date}, nil
 }
